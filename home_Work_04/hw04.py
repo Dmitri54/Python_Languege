b4d7185af67548457295ee7434d048586e21071b
Dmitri5 <dmitriprogram@gmail.com>
2022-09-26 13:54:59 +0700

    I not finished it yet

--- home_Work_04/hw04.py
+++ home_Work_04/hw04.py
@@ -3,25 +3,32 @@
 # Пример:
 # k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 # ----------------------------------------------------------------------------------------------------
-# def foo(x):
-#     return (2 * x) ** 2 + 4 * x + 5
+# Рандом (от 0 до 100) коэффициентов A B C. +
+# k - это x - вводит пользователь.
+# Запись ответа в фаил.
+# ---------------------------------------------------------------------------------------------------
 
-# n = int(input('Введите k: ')) # 2
-# print(foo(n)) # 29
-# -----------------------------------------------------------------
+from pprint import pprint
+from random import randint
+ 
+def random_coefficients(): # Функция создает три рандомных числа .
+    abc = ["A", "B", "C"]
+    tmp = []
+    while len(tmp) < 3:
+        x = randint(0, 101)
+        if x not in tmp:
+           tmp.append(x)
+    return (f'{abc} -> {tmp}'.format(abc, tmp))
 
-# n = int(input())
-# koef = [int(elem) for elem in input().split()]
- 
-# koef.append(koef[-1])
- 
-# for i in range(n, 0, -1):
-#     koef[i] += koef[i-1]
-        
-# print(*koef)
+
+
+number = int(input('Введите число X: '))
+list_abc = (random_coefficients())
+print('Список коеффициентов многочлена: ')
+print(list_abc) # ['A', 'B', 'C'] -> [42, 62, 65]
 
 # with open('/Users/User/Desktop/Python_Language/home_Work_04/file_hw04_hW04.txt', 'a') as f:
-#     f.write(int(*koef))
+#     f.write(int())
+# -------------------------------------
 
-# 5. Даны два файла, в каждом из которых находится запись многочлена.
-# Задача - сформировать файл, содержащий сумму многочленов.
+
