69b11888f746a6f2143b054f6a402e100038aaf9
Dmitri5 <dmitriprogram@gmail.com>
2022-10-12 15:55:48 +0700

    Added libraries for practice

--- seminars/Sem08_telephone_directory/main08.py
+++ seminars/Sem08_telephone_directory/main08.py
@@ -1,2 +1,10 @@
 # Телефонный справочник.
 # Главный Метод.
+
+from isOdd import isOdd
+
+# print(isOdd('1')) //=> true
+# print(isOdd('5')) //=> true
+
+print(isOdd(0)) # //=> false
+print(isOdd(4)) # //=> false
