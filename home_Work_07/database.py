2cee0d9cd7227a2360382034b1222f11b9696604
Dmitri5 <dmitriprogram@gmail.com>
2022-10-15 14:30:26 +0700

    Added home Work 07

---
+++ home_Work_07/database.py
@@ -0,0 +1,42 @@
+
+import psycopg2
+from config import host, user, password, db_name
+
+
+def contact_list(): # Функция, показывает контакты
+    with connection.cursor() as cursor:
+        cursor.execute('select * from contacts;')
+        return cursor.fetchall()
+
+
+def add_contact(lst): # Функция добавляет контакт (Фамилию, Имя, Телефон)
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "insert into contacts (last_name, first_name, phone) values ('{}', '{}', '{}');".format(lst[0],
+                                                                                                    lst[1], lst[2]))
+
+
+def deletion_contact(string): # Функция удаления контакта по Фамилии
+    with connection.cursor() as cursor:
+        cursor.execute("delete from contacts where last_name='{}';".format(string))
+
+
+def contact_finder(string): # Функция поиска контакта (Фамилия и Имя)
+    with connection.cursor() as cursor:
+        cursor.execute("select last_name, first_name from contacts where last_name = '{}'".format(string))
+        print(*cursor.fetchone())
+
+
+def change(last_name, phone): # Функция изменения номера телефона у контакта, по Фамилии.
+    with connection.cursor() as cursor:
+        cursor.execute("UPDATE contacts SET phone = '{}' WHERE last_name = '{}';".format(phone, last_name))
+
+
+connection = psycopg2.connect(host=host,
+                              user=user,
+                              password=password,
+                              database=db_name)
+
+connection.autocommit = True  # авто-коммит - без него не будет обновляться database
+
+change('Шафигуллин', '48165464684')
---
+++ home_Work_07/file_management.py
@@ -0,0 +1,19 @@
+
+import csv
+import database
+import qui
+
+
+def import_data():
+    try:
+        with open(qui.file_name() + '.csv', newline='') as f:
+            reader = list(csv.reader(f, delimiter=';'))
+        [database.add_contact(item) for item in reader]
+    except FileNotFoundError:
+        print('Файл не найден')
+
+
+def write_to_file(lst):
+    with open(qui.file_name() + '.txt', 'w') as f:
+        for item in lst:
+            f.write(', '.join(item) + '\n')
